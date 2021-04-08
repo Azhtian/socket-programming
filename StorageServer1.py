3459919076b2d634941c7f25976fb3af035a13c6
anas <anasalmelhem17@gmail.com>
2021-04-08 23:15:17 +0200

    implemented SQL

--- StorageServer1.py
+++ StorageServer1.py
@@ -1,5 +1,19 @@
 from socket import socket, create_server, AF_INET, SOCK_DGRAM
 from select import select
+from datetime import datetime
+from getpass import getpass
+import mysql.connector
+
+
+def create_dataBase(db_name):
+    my_cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
+
+
+def create_table(station_name):
+    print(f"table created with the name: {station_name}")
+    my_cursor.execute(
+        f"CREATE TABLE IF NOT EXISTS {station_name} (DATA_TIME timestamp PRIMARY KEY, temperature LONGTEXT, precipitation VARCHAR(10));")
+
 
 # Where weather data will be stored
 server_address = ("localhost", 8888)
@@ -14,15 +28,14 @@ tcp_sock.listen()
 
 
 def connect_tcp(sock):
-
     conn, address = sock.accept()
     print("accepted", conn, "from", address)
     req = conn.recv(2048)
 
     available_data = get(req.decode())
-    conn.sendall(available_data)
-    #conn.close()
-    #print(f"From TCP client: {data.decode()}")
+
+    for data in available_data:
+        conn.sendall(f"Time: {data[0]} temperature: {data[1]} precipitation: {data[2]}\n".encode())
 
 
 def connect_udp(sock):
@@ -30,7 +43,8 @@ def connect_udp(sock):
     put(data)
 
 
-def get(entire: str) -> bytes:
+# Fetch data from text file
+def get_from_file(entire: str) -> bytes:
     with open("storage.txt", "rb") as f:
         if entire == "last":
             lines = f.readlines()
@@ -39,23 +53,87 @@ def get(entire: str) -> bytes:
             return f.read()
 
 
-def put(text: bytes) -> None:
+def put_in_file(text: bytes) -> None:
     with open("storage.txt", "ab") as f:
-        f.write(text+b"\n")
+        f.write(text + b"\n")
 
 
-sockets = [tcp_sock, udp_sock]
-
-while True:
-    input_ready, _, _ = select(sockets, [], [])
-    for key in input_ready:
-        if key == tcp_sock:
-            connect_tcp(key)
-
-        elif key == udp_sock:
-            connect_udp(key)
-
-        else:
-            print("unknown socket: ", key)
+def get(entire: str) -> bytes:
+    if entire == "whole":
+        return get_all()
+    elif entire == "last":
+        return get_last()
 
 
+def get_all():
+    my_cursor.execute(f"SELECT * FROM weather.BERGEN;")
+    data = my_cursor.fetchall()
+    print(data)
+    return data
+
+
+def get_last():
+    my_cursor.execute(
+        f"SELECT * FROM WEATHER.Bergen WHERE DATA_TIME = (SELECT MAX(DATA_TIME) FROM WEATHER.Bergen) ;"
+    )
+
+    data = my_cursor.fetchall()
+    print("last data: ", data)
+    return data
+
+
+def put(text: bytes) -> None:
+    message = text.decode()
+    if message[0:3] == "ctb":
+        create_table(message[4:-1])
+    else:
+        putMessage(message)
+
+
+def putMessage(data: str) -> None:
+    # Puts the message into the corresponding table
+    station_name, DATA_TIME, temperature, precipitation = data.split(",")
+
+    DATA_TIME = datetime.strptime(DATA_TIME, "%d/%m/%Y %H:%M:%S")
+    # temp= float(temperature)
+    print(station_name + " ", DATA_TIME, " " + temperature + " " + precipitation)
+    my_cursor.execute(
+        f"INSERT INTO {station_name} VALUES ('{DATA_TIME}','{temperature}','{precipitation}');"
+
+    )
+
+
+if __name__ == '__main__':
+    db_name = input("enter database name: ")
+    print("enter password for connect database: ")
+    # password = input("enter password for connect database: ")
+    password = getpass()
+    SQL_storage = mysql.connector.connect(host='localhost', user='root', password=password, database=db_name)
+    my_cursor = SQL_storage.cursor()
+    create_dataBase("weather")
+
+    # Where weather data will be stored
+    server_address = ("localhost", 8888)
+
+    udp_sock = socket(AF_INET, SOCK_DGRAM)
+
+    udp_sock.bind(("localhost", 8800))
+
+    tcp_sock = socket()
+    tcp_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    tcp_sock.bind(server_address)
+    tcp_sock.listen()
+
+    sockets = [tcp_sock, udp_sock]
+
+    while True:
+        input_ready, _, _ = select(sockets, [], [])
+        for key in input_ready:
+            if key == tcp_sock:
+                connect_tcp(key)
+
+            elif key == udp_sock:
+                connect_udp(key)
+
+            else:
+                print("unknown socket: ", key)
--- WeatherStation1.py
+++ WeatherStation1.py
@@ -5,15 +5,15 @@ from socket import socket, AF_INET, SOCK_DGRAM
 
 sock = socket(AF_INET, SOCK_DGRAM)
 
-station = StationSimulator(simulation_interval=1)
-station.turn_on()
-
+Bergen_station = StationSimulator(simulation_interval=1)
+Bergen_station.turn_on()
+sock.sendto(f"ctb {Bergen_station.location} ".encode(), ("localhost", 8800))
 for _ in range(10):
     sleep(1)
     print("sending new data")
     time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    sock.sendto(f"Station: {station.location } Time: {time}, temperature: {station.temperature}, precipitation: {station.rain}".encode(),
-                ("localhost", 8800))
+    sock.sendto(
+        f"{Bergen_station.location},{time}, {Bergen_station.temperature}, {Bergen_station.rain}".encode(),("localhost", 8800)
+    )
 
-
-station.shut_down()
+Bergen_station.shut_down()
