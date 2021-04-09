b7e6408f5187c2a4b55596619abc995bff5ee1fd
Linh Vu <Linh.Vu@student.uib.no>
2021-04-09 18:54:09 +0200

    added simple template rendered at /

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return "Helllo"
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
